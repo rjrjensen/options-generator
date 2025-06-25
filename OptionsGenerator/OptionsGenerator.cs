2059b9e41492a904ea92c2ccb8785a9d8015fa00
Ryan Jensen <software@rjrjensen.com>
2025-06-24 23:57:13 -0500

    Add CodeAnalysis and start by getting the root node from appsettings.json

---
+++ OptionsGenerator/OptionsGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace OptionsGenerator;
+
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public class OptionsGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var appSettingsFile = context.AdditionalFiles.FirstOrDefault(file => file.Path.EndsWith("appsettings.json"));
+
+        if (appSettingsFile is null) return;
+        
+        var appSettings = appSettingsFile.GetText(context.CancellationToken)?.ToString();
+        
+        if (string.IsNullOrWhiteSpace(appSettings)) return;
+
+        var json = JsonDocument.Parse(appSettings);
+        
+        var obj = json.Deserialize<object> ();
+
+        var root = json.RootElement.;
+    }
+}
