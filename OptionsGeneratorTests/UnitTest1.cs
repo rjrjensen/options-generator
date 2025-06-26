3694be04bb0f18ab97a64e5d9bcb464c583c3c95
Ryan Jensen <software@rjrjensen.com>
2025-06-25 23:18:21 -0500

    Add sample unit test code

--- OptionsGeneratorTests/UnitTest1.cs
+++ OptionsGeneratorTests/UnitTest1.cs
@@ -1,10 +1,24 @@
 ﻿namespace OptionsGeneratorTests;
 
+using VerifyCS = CSharpSourceGeneratorVerifier<YourGenerator>; And use the following in your test method:
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        var code = "initial code";
+        var generated = "expected generated code";
+        await new VerifyCS.Test
+        {
+            TestState = 
+            {
+                Sources = { code },
+                GeneratedSources =
+                {
+                    (typeof(YourGenerator), "GeneratedFileName", SourceText.From(generated, Encoding.UTF8, SourceHashAlgorithm.Sha256)),
+                },
+            },
+        }.RunAsync();
     }
 }
