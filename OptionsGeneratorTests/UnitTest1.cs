bb03195c37882d7a3aee2b74ba51994ff74e829f
Ryan Jensen <software@rjrjensen.com>
2025-06-26 22:55:20 -0500

    Add sample CSharpSourceGeneratorVerifier

--- OptionsGeneratorTests/UnitTest1.cs
+++ OptionsGeneratorTests/UnitTest1.cs
@@ -1,6 +1,12 @@
 ﻿namespace OptionsGeneratorTests;
 
-using VerifyCS = CSharpSourceGeneratorVerifier<YourGenerator>; And use the following in your test method:
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using OptionsGenerator;
+using VerifyCS = CSharpSourceGeneratorVerifier<YourGenerator>; 
 
 public class UnitTest1
 {
@@ -16,9 +22,43 @@ public class UnitTest1
                 Sources = { code },
                 GeneratedSources =
                 {
-                    (typeof(YourGenerator), "GeneratedFileName", SourceText.From(generated, Encoding.UTF8, SourceHashAlgorithm.Sha256)),
+                    (typeof(OptionsGenerator), "GeneratedFileName", SourceText.From(generated, Encoding.UTF8, SourceHashAlgorithm.Sha256)),
                 },
             },
         }.RunAsync();
+    }
+}
+
+public static class CSharpSourceGeneratorVerifier<TSourceGenerator>
+    where TSourceGenerator : ISourceGenerator, new()
+{
+    public class Test : CSharpSourceGeneratorTest<TSourceGenerator, XUnitVerifier>
+    {
+        public Test()
+        {
+        }
+
+        protected override CompilationOptions CreateCompilationOptions()
+        {
+            var compilationOptions = base.CreateCompilationOptions();
+            return compilationOptions.WithSpecificDiagnosticOptions(
+                compilationOptions.SpecificDiagnosticOptions.SetItems(GetNullableWarningsFromCompiler()));
+        }
+
+        public LanguageVersion LanguageVersion { get; set; } = LanguageVersion.Default;
+
+        private static ImmutableDictionary<string, ReportDiagnostic> GetNullableWarningsFromCompiler()
+        {
+            string[] args = { "/warnaserror:nullable" };
+            var commandLineArguments = CSharpCommandLineParser.Default.Parse(args, baseDirectory: Environment.CurrentDirectory, sdkDirectory: Environment.CurrentDirectory);
+            var nullableWarnings = commandLineArguments.CompilationOptions.SpecificDiagnosticOptions;
+
+            return nullableWarnings;
+        }
+
+        protected override ParseOptions CreateParseOptions()
+        {
+            return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(LanguageVersion);
+        }
     }
 }
